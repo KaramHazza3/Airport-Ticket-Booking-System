fa26c21767d825e00bd6ffc80433c6e25d739086
Karam Hazzaa <karam.hazzaa@gmail.com>
2025-08-01 05:22:50 +0300

    Add reusable helper for filter

---
+++ Common/FilterHelper.cs
@@ -0,0 +1,18 @@
+﻿using FTSAirportTicketBookingSystem.Common.Services;
+namespace FTSAirportTicketBookingSystem.Common;
+
+public static class FilterHelper
+{
+    public static async Task<Result<List<T>>> FilterAsync<T>(
+        IBaseService<T> baseService,
+        params Func<T, bool>[] predicates)
+    {
+        var result = await baseService.GetAllAsync();
+        if (result.IsFailure)
+            return result.Error;
+
+        var resultList = result.Value;
+        Func<T, bool> combinedMatches = f => predicates.All(predicate => predicate(f));
+        return resultList.Where(combinedMatches).ToList();
+    }
+}
